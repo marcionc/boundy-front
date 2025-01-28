9ea6460abe9d403774f1d328bb7df5e410350195
Marcio Nunes <marcio.nunes.c@gmail.com>
2025-01-28 12:45:03 -0300

    Configurar título, favicon e domínio personalizado

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,20 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' 
+    ? 'https://bounky.com.br/' 
+    : '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
